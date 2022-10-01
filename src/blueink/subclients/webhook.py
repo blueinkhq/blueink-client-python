fe131ee22f71b9ed3d515c587e5dd47000642012
Joe Sheldon <joe.sheldon@blueink.com>
2022-09-30 17:55:18 -0700

    webhook subclient filled out

--- src/blueink/subclients/webhook.py
+++ src/blueink/subclients/webhook.py
@@ -1,6 +1,8 @@
 from typing import List
 
+from src.blueink import endpoints
 from src.blueink.model.webhook import WebhookExtraHeader
+from src.blueink.request_helper import NormalizedResponse
 from src.blueink.subclients.subclient import SubClient
 
 
@@ -11,64 +13,106 @@ class WebhookSubClient(SubClient):
     # ----------
     # Webhooks
     # ----------
-    def create_webhook(self, url: str, event_types:List[str],
-                       extra_headers: List[WebhookExtraHeader], enabled: bool = True,
-                       json: bool = True):
-        raise RuntimeError("Not Implemented")
+    def create_webhook(self, data: dict):
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.CREATE)
 
-    def list_webhooks(self):
-        raise RuntimeError("Not Implemented")
+        return self._requests.post(url, data=data)
 
-    def retrieve_webhook(self, webhook_id: str):
-        raise RuntimeError("Not Implemented")
+    def list_webhooks(self, **query_params) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.LIST)
 
-    def delete_webhook(self, webhook_id: str):
-        raise RuntimeError("Not Implemented")
+        return self._requests.get(url,
+                                  params=self.build_params(query_params=query_params))
 
-    def update_webhook(self, webhook_id: str, **data):
-        raise RuntimeError("Not Implemented")
+    def retrieve_webhook(self, webhook_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.RETRIEVE,
+                             webhook_id=webhook_id)
+
+        return self._requests.get(url)
+
+    def delete_webhook(self, webhook_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.DELETE,
+                             webhook_id=webhook_id)
+
+        return self._requests.delete(url)
+
+    def update_webhook(self, webhook_id: str, data: dict) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.UPDATE,
+                             webhook_id=webhook_id)
+
+        return self._requests.patch(url, data=data)
 
     # ----------
     # Extra Header
     # ----------
-    def create_header(self, webhook_id: str, name: str, value: str, order: int):
-        raise RuntimeError("Not Implemented")
+    def create_header(self, data: dict) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.CREATE_HEADER)
 
-    def list_headers(self):
-        raise RuntimeError("Not Implemented")
+        return self._requests.post(url, data=data)
 
-    def retrieve_header(self, header_id: str):
-        raise RuntimeError("Not Implemented")
+    def list_headers(self, **query_params) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.LIST_HEADERS)
 
-    def delete_header(self, header_id: str):
-        raise RuntimeError("Not Implemented")
+        return self._requests.get(url,
+                                  params=self.build_params(query_params=query_params))
 
-    def update_header(self, header_id: str, **data):
-        raise RuntimeError("Not Implemented")
+    def retrieve_header(self, header_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.RETRIEVE_HEADER,
+                             webhook_header_id=header_id)
+
+        return self._requests.get(url)
+
+    def delete_header(self, header_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.DELETE_HEADERE,
+                             webhook_header_id=header_id)
+
+        return self._requests.delete(url)
+
+    def update_header(self, header_id: str, **data) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.UPDATE_HEADER,
+                             webhook_header_id=header_id)
+
+        return self._requests.patch(url, data=data)
 
     # ----------
     # Events
     # ----------
-    def list_events(self):
-        raise RuntimeError("Not Implemented")
+    def list_events(self, **query_params) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.LIST_EVENTS)
 
-    def retrieve_event(self, event_id: str):
-        raise RuntimeError("Not Implemented")
+        return self._requests.get(url,
+                                  params=self.build_params(query_params=query_params))
+
+    def retrieve_event(self, event_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.RETRIEVE_EVENT,
+                             wehook_event_id=event_id)
+
+        return self._requests.get(url)
 
     # ----------
     # Deliveries
     # ----------
-    def list_deliveries(self):
-        raise RuntimeError("Not Implemented")
+    def list_deliveries(self, **query_params) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.LIST_DELIVERIES)
 
-    def retrieve_delivery(self, event_id: str):
-        raise RuntimeError("Not Implemented")
+        return self._requests.get(url,
+                                  params=self.build_params(query_params=query_params))
+
+    def retrieve_delivery(self, delivery_id: str) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.RETRIEVE_DELIVERY,
+                             webhook_delivery_id=delivery_id)
+
+        return self._requests.get(url)
 
     # ----------
     # Secret
     # ----------
-    def retrieve_secret(self):
-        raise RuntimeError("Not Implemented")
+    def retrieve_secret(self) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.RETRIEVE_SECRET)
 
-    def regenerate_secret(self):
-        raise RuntimeError("Not Implemented")
+        return self._requests.get(url)
+
+    def regenerate_secret(self) -> NormalizedResponse:
+        url = self.build_url(endpoint=endpoints.WEBHOOKS.REGENERATE_SECRET)
+
+        return self._requests.post(url)
